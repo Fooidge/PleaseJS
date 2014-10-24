feebad09dae884ba5d8814219857b7485ecc5654
Jordan Checkman <jordan.checkman@gmail.com>
2014-10-24 18:59:33 -0400

    pt2

--- node_modules/gulp-jshint/test/specs/integration/index.js
+++
@@ -1,6 +0,0 @@
-describe('integration', function () {
-  require('./basic');
-  require('./extraction');
-  require('./file-ignores');
-  require('./overrides');
-});
--- node_modules/gulp/node_modules/vinyl-fs/node_modules/through2/node_modules/readable-stream/readable.js
+++
@@ -1,7 +0,0 @@
-require('stream'); // hack to fix a circular dependency issue when used with browserify
-exports = module.exports = require('./lib/_stream_readable.js');
-exports.Readable = exports;
-exports.Writable = require('./lib/_stream_writable.js');
-exports.Duplex = require('./lib/_stream_duplex.js');
-exports.Transform = require('./lib/_stream_transform.js');
-exports.PassThrough = require('./lib/_stream_passthrough.js');
