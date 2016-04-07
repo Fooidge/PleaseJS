2d0729a7169850e0bc960aac7e96db87b21a6532
Jason Manuel <jama.indo@hotmail.com>
2016-04-07 21:48:57 +0900

    Rewrite make_color tests to test by example
    While random data did seem like a good idea, unless you're using
    a quicktest equivalent it just makes your test ugly. All tests from
    now on will be by example.
    
    Lesson learned. (To see the lesson, go back.)

--- spec/make_color.spec.js
+++ spec/make_color.spec.js
@@ -1,56 +1,7 @@
 describe("Please.make_color()",
   function (){
 
-    /* Arbitrary maximum in colors_returned tests. */
-    var MAX_COLORS_RETURNED = 256;
-
-    var calcHue = function (color){
-      var rgb = {
-        r: parseInt(color.slice(1, 3), 16),
-        g: parseInt(color.slice(3, 5), 16),
-        b: parseInt(color.slice(5), 16)
-      };
-
-      var calculatedHue = Math.atan2(Math.sqrt(3) * (rgb.g - rgb.b),
-        2 * rgb.r - rgb.g - rgb.b);
-
-      /* Make negative angles positive. */
-      if (calculatedHue < 0)
-      {
-        calculatedHue += 2 * Math.PI;
-      }
-
-      return calculatedHue * 180 / Math.PI;
-    };
-
-    var calcSat = function (color){
-      var rgb = {
-        r: parseInt(color.slice(1, 3), 16),
-        g: parseInt(color.slice(3, 5), 16),
-        b: parseInt(color.slice(5), 16)
-      };
-
-      var chroma = Math.max(rgb.r, rgb.g, rgb.b)
-        - Math.min(rgb.r, rgb.g, rgb.b);
-      var value = Math.max(rgb.r, rgb.g, rgb.b);
-      var calculatedSat = 0;
-      if (value != 0)
-      {
-        calculatedSat = chroma / value;
-      }
-
-      return calculatedSat;
-    };
-
-    var calcValue = function (color){
-      var rgb = {
-        r: parseInt(color.slice(1, 3), 16),
-        g: parseInt(color.slice(3, 5), 16),
-        b: parseInt(color.slice(5), 16)
-      };
-
-      return Math.max(rgb.r, rgb.g, rgb.b)/255;
-    };
+    // TODO: replace calc* with this.calc*
 
     it("with default arguments",
       function (){
@@ -59,14 +10,14 @@ describe("Please.make_color()",
         expect(hexRegex.test(color)).toBe(true);
 
         /* I don't know how to test the hue. */
-        expect(calcSat(color)).toBeCloseTo(0.4, 0);
-        expect(calcValue(color)).toBeCloseTo(0.75, 0);
+        expect(this.calcSat(color)).toBeCloseTo(0.4, 0);
+        expect(this.calcValue(color)).toBeCloseTo(0.75, 0);
       }
      );
 
     it("with predetermined hue",
       function (){
-        var hue = Math.floor(Math.random() * 360); /* Random hue. */
+        var hue = 0;
 
         var color = Please.make_color(
           {
@@ -74,220 +25,54 @@ describe("Please.make_color()",
             hue: hue
           })[0];
 
-        expect(calcHue(color)).toBeCloseTo(hue, -1);
+        expect(this.calcHue(color)).toBeCloseTo(hue, -1);
       }
      );
 
     it("with predetermined saturation",
       function (){
-        var saturation = Math.random();
+        var saturation = 0.6;
         var color = Please.make_color(
           {
             saturation: saturation
           })[0];
 
-        expect(calcSat(color)).toBeCloseTo(saturation, 1);
+        expect(this.calcSat(color)).toBeCloseTo(saturation, 1);
       }
      );
 
     it("with predetermined value",
       function (){
-        var value = Math.random();
+        var value = 0.5;
         var color = Please.make_color(
           {
             value: value
           })[0];
 
 
-        expect(calcValue(color)).toBeCloseTo(value, 0);
+        expect(this.calcValue(color)).toBeCloseTo(value, 0);
       }
      );
 
     /* This occasionally fails for now. HELP WANTED. */
     it("with a base color",
       function (){
-        var colors = [
-          ["aliceblue", "F0F8FF"],
-    			["antiquewhite", "FAEBD7"],
-    			["aqua", "00FFFF"],
-    			["aquamarine", "7FFFD4"],
-    			["azure", "F0FFFF"],
-    			["beige", "F5F5DC"],
-    			["bisque", "FFE4C4"],
-    			["black", "000000"],
-    			["blanchedalmond", "FFEBCD"],
-    			["blue", "0000FF"],
-    			["blueviolet", "8A2BE2"],
-    			["brown", "A52A2A"],
-    			["burlywood", "DEB887"],
-    			["cadetblue", "5F9EA0"],
-    			["chartreuse", "7FFF00"],
-    			["chocolate", "D2691E"],
-    			["coral", "FF7F50"],
-    			["cornflowerblue", "6495ED"],
-    			["cornsilk", "FFF8DC"],
-    			["crimson", "DC143C"],
-    			["cyan", "00FFFF"],
-    			["darkblue", "00008B"],
-    			["darkcyan", "008B8B"],
-    			["darkgoldenrod", "B8860B"],
-    			["darkgray", "A9A9A9"],
-    			["darkgrey", "A9A9A9"],
-    			["darkgreen", "006400"],
-    			["darkkhaki", "BDB76B"],
-    			["darkmagenta", "8B008B"],
-    			["darkolivegreen", "556B2F"],
-    			["darkorange", "FF8C00"],
-    			["darkorchid", "9932CC"],
-    			["darkred", "8B0000"],
-    			["darksalmon", "E9967A"],
-    			["darkseagreen", "8FBC8F"],
-    			["darkslateblue", "483D8B"],
-    			["darkslategray", "2F4F4F"],
-    			["darkslategrey", "2F4F4F"],
-    			["darkturquoise", "00CED1"],
-    			["darkviolet", "9400D3"],
-    			["deeppink", "FF1493"],
-    			["deepskyblue", "00BFFF"],
-    			["dimgray", "696969"],
-    			["dimgrey", "696969"],
-    			["dodgerblue", "1E90FF"],
-    			["firebrick", "B22222"],
-    			["floralwhite", "FFFAF0"],
-    			["forestgreen", "228B22"],
-    			["fuchsia", "FF00FF"],
-    			["gainsboro", "DCDCDC"],
-    			["ghostwhite", "F8F8FF"],
-    			["gold", "FFD700"],
-    			["goldenrod", "DAA520"],
-    			["gray", "808080"],
-    			["grey", "808080"],
-    			["green", "008000"],
-    			["greenyellow", "ADFF2F"],
-    			["honeydew", "F0FFF0"],
-    			["hotpink", "FF69B4"],
-    			["indianred", "CD5C5C"],
-    			["indigo", "4B0082"],
-    			["ivory", "FFFFF0"],
-    			["khaki", "F0E68C"],
-    			["lavender", "E6E6FA"],
-    			["lavenderblush", "FFF0F5"],
-    			["lawngreen", "7CFC00"],
-    			["lemonchiffon", "FFFACD"],
-    			["lightblue", "ADD8E6"],
-    			["lightcoral", "F08080"],
-    			["lightcyan", "E0FFFF"],
-    			["lightgoldenrodyellow", "FAFAD2"],
-    			["lightgray", "D3D3D3"],
-    			["lightgrey", "D3D3D3"],
-    			["lightgreen", "90EE90"],
-    			["lightpink", "FFB6C1"],
-    			["lightsalmon", "FFA07A"],
-    			["lightseagreen", "20B2AA"],
-    			["lightskyblue", "87CEFA"],
-    			["lightslategray", "778899"],
-    			["lightslategrey", "778899"],
-    			["lightsteelblue", "B0C4DE"],
-    			["lightyellow", "FFFFE0"],
-    			["lime", "00FF00"],
-    			["limegreen", "32CD32"],
-    			["linen", "FAF0E6"],
-    			["magenta", "FF00FF"],
-    			["maroon", "800000"],
-    			["mediumaquamarine", "66CDAA"],
-    			["mediumblue", "0000CD"],
-    			["mediumorchid", "BA55D3"],
-    			["mediumpurple", "9370D8"],
-    			["mediumseagreen", "3CB371"],
-    			["mediumslateblue", "7B68EE"],
-    			["mediumspringgreen", "00FA9A"],
-    			["mediumturquoise", "48D1CC"],
-    			["mediumvioletred", "C71585"],
-    			["midnightblue", "191970"],
-    			["mintcream", "F5FFFA"],
-    			["mistyrose", "FFE4E1"],
-    			["moccasin", "FFE4B5"],
-    			["navajowhite", "FFDEAD"],
-    			["navy", "000080"],
-    			["oldlace", "FDF5E6"],
-    			["olive", "808000"],
-    			["olivedrab", "6B8E23"],
-    			["orange", "FFA500"],
-    			["orangered", "FF4500"],
-    			["orchid", "DA70D6"],
-    			["palegoldenrod", "EEE8AA"],
-    			["palegreen", "98FB98"],
-    			["paleturquoise", "AFEEEE"],
-    			["palevioletred", "D87093"],
-    			["papayawhip", "FFEFD5"],
-    			["peachpuff", "FFDAB9"],
-    			["peru", "CD853F"],
-    			["pink", "FFC0CB"],
-    			["plum", "DDA0DD"],
-    			["powderblue", "B0E0E6"],
-    			["purple", "800080"],
-    			["rebeccapurple", "663399"],
-    			["red", "FF0000"],
-    			["rosybrown", "BC8F8F"],
-    			["royalblue", "4169E1"],
-    			["saddlebrown", "8B4513"],
-    			["salmon", "FA8072"],
-    			["sandybrown", "F4A460"],
-    			["seagreen", "2E8B57"],
-    			["seashell", "FFF5EE"],
-    			["sienna", "A0522D"],
-    			["silver", "C0C0C0"],
-    			["skyblue", "87CEEB"],
-    			["slateblue", "6A5ACD"],
-    			["slategray", "708090"],
-    			["slategrey", "708090"],
-    			["snow", "FFFAFA"],
-    			["springgreen", "00FF7F"],
-    			["steelblue", "4682B4"],
-    			["tan", "D2B48C"],
-    			["teal", "008080"],
-    			["thistle", "D8BFD8"],
-    			["tomato", "FF6347"],
-    			["turquoise", "40E0D0"],
-    			["violet", "EE82EE"],
-    			["wheat", "F5DEB3"],
-    			["white", "FFFFFF"],
-    			["whitesmoke", "F5F5F5"],
-    			["yellow", "FFFF00"],
-    			["yellowgreen", "9ACD32"]
-        ];
-        var index = Math.floor(Math.random() * colors.length);
         var generatedColor = Please.make_color(
           {
-            base_color: colors[index][0]
+            base_color: 'red'
           })[0];
-        var approxEqual = function (a, b, error){
-          return Math.abs(a - b) <= error;
-        };
-        var correctHue = calcHue("#" + colors[index][1]);
-        var cond = [];
 
-        cond[0] = approxEqual(correctHue - 5, calcHue(generatedColor),
-          10);
-        cond[1] = approxEqual(correctHue + 5, calcHue(generatedColor),
-          10);
-        cond[2] = approxEqual(0.4, calcSat(generatedColor), 10);
-        cond[3] = approxEqual(0.85, calcSat(generatedColor), 10);
-        cond[4] = approxEqual(0.4, calcValue(generatedColor), 10);
-        cond[5] = approxEqual(0.85, calcValue(generatedColor), 10);
+        // Correct hue
+        var hue = this.calcHue(generatedColor);
+        expect(0 <= hue || hue <= 5).toBe(true);
 
-        expect((correctHue - 5 <= calcHue(generatedColor)) || cond[0])
-          .toBe(true);
-        expect((correctHue + 5 >= calcHue(generatedColor)) || cond[1])
-          .toBe(true);
+        // Correct saturation
+        var sat = this.calcSat(generatedColor);
+        expect(.4 <= sat || sat <= .85).toBe(true);
 
-        expect((.4 <= calcSat(generatedColor)) || cond[2]).toBe(true);
-        expect((.85 >= calcSat(generatedColor)) || cond[3]).toBe(true);
-
-        expect((.4 <= calcValue(generatedColor)) || cond[4])
-          .toBe(true);
-        expect((.85 >= calcValue(generatedColor)) || cond[5])
-          .toBe(true);
+        // Correct value
+        var value = this.calcValue(generatedColor);
+        expect(.4 <= value || value <= .85).toBe(true);
       }
      );
 
@@ -300,7 +85,7 @@ describe("Please.make_color()",
             ? (options.grayscale = true) : (options.greyscale = true);
             var color = Please.make_color(options)[0];
 
-            expect(calcSat(color)).toBeCloseTo(0);
+            expect(this.calcSat(color)).toBeCloseTo(0);
           }
         });
 
@@ -354,8 +139,8 @@ describe("Please.make_color()",
 
         expect(hexRegex.test(color)).toBe(true);
 
-        expect(calcSat(color)).toBeCloseTo(0.4, 0);
-        expect(calcValue(color)).toBeCloseTo(0.75, 0);
+        expect(this.calcSat(color)).toBeCloseTo(0.4, 0);
+        expect(this.calcValue(color)).toBeCloseTo(0.75, 0);
       }
      );
 
@@ -369,8 +154,8 @@ describe("Please.make_color()",
         expect(hexRegex.test(color)).toBe(true);
 
         /* I don't know how to test the hue. */
-        expect(calcSat(color)).toBeCloseTo(0.4, 0);
-        expect(calcValue(color)).toBeCloseTo(0.75, 0);
+        expect(this.calcSat(color)).toBeCloseTo(0.4, 0);
+        expect(this.calcValue(color)).toBeCloseTo(0.75, 0);
       }
      );
 
@@ -395,22 +180,22 @@ describe("Please.make_color()",
         expect(hexRegex.test(color)).toBe(true);
 
         /* I don't know how to test the hue. */
-        expect(calcSat(color)).toBeCloseTo(0.4, 0);
-        expect(calcValue(color)).toBeCloseTo(0.75, 0);
+        expect(this.calcSat(color)).toBeCloseTo(0.4, 0);
+        expect(this.calcValue(color)).toBeCloseTo(0.75, 0);
       }
      );
 
     it("with colors_returned explicitly set",
       function (){
         var colors_returned
-          = Math.ceil(Math.random() * MAX_COLORS_RETURNED);
+          = 256;
         var colors = Please.make_color(
           {
             colors_returned: colors_returned
           });
         var hexRegex = /^#[0-9a-fA-F]{6}$/;
 
-        colors = colors instanceof Array ? colors : [colors];
+        expect(colors.length).toEqual(256);
 
         for (var i = 0; i < colors.length; i++)
         {
@@ -418,8 +203,8 @@ describe("Please.make_color()",
           expect(hexRegex.test(colors[i])).toBe(true);
 
           /* I don't know how to test the hue. */
-          expect(calcSat(colors[i])).toBeCloseTo(0.4, 0);
-          expect(calcValue(colors[i])).toBeCloseTo(0.75, 0);
+          expect(this.calcSat(colors[i])).toBeCloseTo(0.4, 0);
+          expect(this.calcValue(colors[i])).toBeCloseTo(0.75, 0);
         }
       }
      );
